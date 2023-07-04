ae166a2a0359b4ab12cf882136b51fef5c364536
Pixel Frame <pm421@live.com>
2023-07-04 18:54:20 +0800

    Implement ToString for Copy

---
+++ Helper.cs
@@ -0,0 +1,14 @@
+﻿namespace KzA.Blazor.HexDump
+{
+    internal static class Helper
+    {
+        internal static char AsciiFromByte(byte b)
+        {
+            if (b < 0x21 || b > 0x7E)
+            {
+                return '.';
+            }
+            return (char)b;
+        }
+    }
+}
